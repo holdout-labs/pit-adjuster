f7bb357a40bf7c324a50f0cd9620879f1dde3208
tongflau-dongzhu <tong.f.lau@gmail.com>
2026-09-05 00:37:17 +0800

    chore: sync __version__ to pyproject (CLI version output was stale)

--- src/pit_adjuster/__init__.py
+++ src/pit_adjuster/__init__.py
@@ -11,7 +11,7 @@ Zero dependencies. Python 3.11+.
 from .chain import build_multipliers, events_from_actions, rebuild_bars
 from .validation import compare_raw_closes, compare_snapshots, validate_inversion
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     "build_multipliers",
